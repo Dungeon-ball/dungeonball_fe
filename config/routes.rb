69c5426975195aa87e25554320aad47379a84421
Alex Psitos <apsitos08@gmail.com>
2022-06-04 15:22:23 -0400

    Feat: create Users table with Twitter columns

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get '/register', to: 'users#new'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  # get '/login', to: 'sessions#new'
+  # post '/login', to: 'sessions#create'
+  # get '/logout', to: 'sessions#destroy'
+  get '/auth/twitter', as: 'twitter_login'
+  get '/auth/twitter/callback', to: 'sessions#create'
   get '/search/players', to: 'searches#index'
   get '/player/show/:id', to: 'players#show'
   resources :users, only: [:create]
