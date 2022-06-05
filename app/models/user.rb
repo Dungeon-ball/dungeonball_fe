5e44e8f74cc06122b736785021550be1a381ded6
Alex Psitos <apsitos08@gmail.com>
2022-06-05 11:23:31 -0400

    WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
       new_user.screen_name        = auth_info.extra.access_token.params[:screen_name]
       new_user.oauth_token        = auth_info.extra.access_token.token
       new_user.oauth_token_secret = auth_info.extra.access_token.secret
+      # new_user.oauth_token        = auth_info.extra.access_token.params[:oauth_token]
+      # new_user.oauth_token_secret = auth_info.info.access_token.params[:oauth_token_secret]
     end
   end
 end
