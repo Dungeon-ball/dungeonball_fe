2158453e82d2a7b524a093e45beff0e5e3a5913c
Alex Psitos <apsitos08@gmail.com>
2022-06-06 19:16:14 -0400

    Test: player service test written and passes spec

---
+++ spec/services/player_service.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe PlayerService do
+  it '#get_all_players_by_name' do
+    json_response = File.read("spec/fixtures/players_index.json")
+    stub_request(:get, "http://localhost:3000/api/v1/players?query=timmy").to_return(status: 200, body: json_response)
+    get_players = PlayerService.get_all_players_by_name("timmy")
+
+    expect(get_players).to be_a(Hash)
+    expect(get_players[:data]).to be_a(Array)
+    expect(get_players[:data].count).to eq(3)
+    expect(get_players[:data][0][:attributes][:name]).to eq("Timmy Thompson")
+    expect(get_players[:data][1][:attributes][:name]).to eq("Thompson Stimmy")
+    expect(get_players[:data][2][:attributes][:name]).to eq("Alex Pstimmy")
+  end
+end
