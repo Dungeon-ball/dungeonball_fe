d9ded74266741748398916efd7e8de89f22a53e5
Drew Proebstel <d.proebstel@gmail.com>
2022-06-08 16:03:24 -0700

    get player by id service

--- spec/services/player_service.rb
+++ spec/services/player_service.rb
@@ -13,4 +13,15 @@ RSpec.describe PlayerService do
     expect(get_players[:data][1][:attributes][:name]).to eq("Thompson Stimmy")
     expect(get_players[:data][2][:attributes][:name]).to eq("Alex Pstimmy")
   end
+
+  it '#get_player_by_id'  do
+    json_response = File.read("spec/fixtures/players_show.json")
+    stub_request(:get, "http://localhost:3000/api/v1/players/1").to_return(status: 200, body: json_response)
+    player = PlayerService.get_player_by_id("1")
+
+    expect(player).to be_a(Hash)
+    expect(player[:data][:type]).to eq("player")
+    expect(player[:data][:attributes].length).to eq(8)
+    expect(player[:data][:attributes][:dexterity]).to eq(14)
+  end
 end
