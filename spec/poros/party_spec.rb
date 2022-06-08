8203df8bbdc6f770055f9ee4fd9c576cee637c6d
Drew Proebstel <d.proebstel@gmail.com>
2022-06-08 15:43:55 -0700

    party spec

--- spec/poros/party_spec.rb
+++ spec/poros/party_spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe PlayerIndex do
-  let(:drew) do
-    PlayerIndex.new({
+RSpec.describe Party do
+  let(:drews_party) do
+    Party.new({
       id: "4",
-      name: "Drew"
+      attributes:{
+      name: "Drews Party",
+      user_id: "123"}
 
 })
 
   end
   it 'has attributes' do
-    expect(drew).to be_an_instance_of(PlayerIndex)
-    expect(drew.id).to eq("4")
-    expect(drew.name).to eq("Drew")
+    expect(drews_party).to be_an_instance_of(Party)
+    expect(drews_party.id).to eq("4")
+    expect(drews_party.name).to eq("Drews Party")
+    expect(drews_party.user_id).to eq("123")
   end
 end
